d4a17dd3668a2441abe6816ec13db708885ef735
Chris <christophpryer@gmail.com>
2019-11-27 11:37:18 -0500

    added initial configuration

--- dfis/__init__.py
+++ dfis/__init__.py
@@ -1,6 +1,45 @@
 import os
+import sys
+
+import pandas as pd
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 class Config:
     def __init__(self, module):
-        self.module = module
+        self.module = module # main.py or run.py __name__
         self.root = os.path.dirname(os.path.abspath(module))
+        self.setup()
+
+    def setup(self):
+        self.levels = pd.DataFrame() # algo config data
+        self.data = pd.DataFrame() # input data
+        self.storage = '' # path to storage directory
+        
+        self.info = pd.DataFrame()
+        config_name = ''
+        if 'config.csv' in os.listdir(self.root):
+            config_name = 'config.csv'
+            self.info = pd.read_csv(os.path.join(self.root, config_name))
+        elif 'testing_config.csv' in os.listdir(self.root):
+            config_name = 'testing_config.csv'
+            self.info = pd.read_csv(os.path.join(self.root, config_name))
+        
+        if len(self.info) > 1:
+            logging.warn('only the first row was used in %s.' % config_name)
+        
+        if not self.info.empty:
+            # currently expects everything to be relative to self.root.
+            self.levels = pd.read_csv(os.path.join(self.root, self.info.levels.iloc[0]))
+            self.data = pd.read_csv(os.path.join(self.root, self.info.data.iloc[0]))
+            self.storage = os.path.join(self.root, self.info.storage.iloc[0])
+
+        else:
+            logging.warn('levels, data, and storage have not been configured: %s' % self.to_dict)
+
+    def to_dict(self):
+        return {
+            'root': self.root,
+            'info': self.info.to_dict()
+        }
--- tests.py
+++ tests.py
@@ -1,7 +1,17 @@
 import os
+import sys
 
 from dfis import Config
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 def test_config_init():
     config = Config(__name__)
+    logging.info(config.to_dict())
+
     assert os.path.dirname(os.path.abspath(__name__)) == config.root
+
+    assert not config.levels.empty
+    assert not config.data.empty
+    assert config.storage != ''
