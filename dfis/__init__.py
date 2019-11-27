eeab8b89e4d79467ff5cf6a7ea7fd1baf12614d6
Chris <christophpryer@gmail.com>
2019-11-27 11:46:05 -0500

    refactored Config

--- dfis/__init__.py
+++ dfis/__init__.py
@@ -12,32 +12,36 @@ class Config:
         self.root = os.path.dirname(os.path.abspath(module))
         self.setup()
 
+        if self.info.empty:
+            logging.warn('levels, data, and storage need configuration: %s' % self.to_dict)
+        elif len(self.info) > 1:
+            logging.warn('only the first row was used in %s.' % config_name)
+
+    @staticmethod
+    def get_config_info(path):
+        """takes path (directory); returns dataframe"""
+        info_name = ''
+        info_data = pd.DataFrame()
+        if 'config.csv' in os.listdir(self.root):
+            config_name = 'config.csv'
+            info_data = pd.read_csv(os.path.join(path, info_name))
+        elif 'testing_config.csv' in os.listdir(self.root):
+            config_name = 'testing_config.csv'
+            info_data = pd.read_csv(os.path.join(path, info_name))
+        return info_data
+        
     def setup(self):
         self.levels = pd.DataFrame() # algo config data
         self.data = pd.DataFrame() # input data
         self.storage = '' # path to storage directory
-        
-        self.info = pd.DataFrame()
-        config_name = ''
-        if 'config.csv' in os.listdir(self.root):
-            config_name = 'config.csv'
-            self.info = pd.read_csv(os.path.join(self.root, config_name))
-        elif 'testing_config.csv' in os.listdir(self.root):
-            config_name = 'testing_config.csv'
-            self.info = pd.read_csv(os.path.join(self.root, config_name))
-        
-        if len(self.info) > 1:
-            logging.warn('only the first row was used in %s.' % config_name)
-        
+        self.info = self.get_config_info(self.root)
+
         if not self.info.empty:
             # currently expects everything to be relative to self.root.
             self.levels = pd.read_csv(os.path.join(self.root, self.info.levels.iloc[0]))
             self.data = pd.read_csv(os.path.join(self.root, self.info.data.iloc[0]))
             self.storage = os.path.join(self.root, self.info.storage.iloc[0])
-
-        else:
-            logging.warn('levels, data, and storage have not been configured: %s' % self.to_dict)
-
+            
     def to_dict(self):
         return {
             'root': self.root,
