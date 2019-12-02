5f3f415a85311c935d8c328ea151775a9022c617
christopherpryer <christophpryer@gmail.com>
2019-12-02 09:49:04 -0500

    improved tests

--- tests.py
+++ tests.py
@@ -1,10 +1,17 @@
 import os
 import sys
+import shutil
 
 from dfis import Config, App
 import logging
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+def reset_instance(obj):
+    """NOTE: this will wipe out all contents within app.storage"""
+    logging.info('removing %s contents from %s.' % (len(os.listdir(obj.storage)), obj.storage))
+    shutil.rmtree(obj.storage)
+    os.makedirs(obj.storage)
 
 def check_instance(obj):
     logging.info(obj.info_to_dict())
@@ -31,6 +38,7 @@ def test_app_run():
     assert len(app.get_attributes()) >= 1
     assert len(list(app.results)) > 0
 
+    reset_instance(app)
     app.save(app.results)
 
     assert os.path.exists(os.path.join(app.root, app.storage))
